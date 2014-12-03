c76b13ea5e3c185dce2add40f5c1448cd48d6414
AndersHqst <anders.host.kjaergaard@gmail.com>
2014-12-03 15:58:47 +0100

    Cleanup. Removed unused code and comments. Moved memoisation to seperate file

--- bin/mtv.py
+++ bin/mtv.py
@@ -5,29 +5,20 @@ from math import log
 from itertools import combinations
 import copy
 from time import time
-
-def memoise(f):
-    """ Memoization decorator for functions taking one or more arguments. """
-    class memodict(dict):
-        def __init__(self, f):
-            self.f = f
-        def __call__(self, *args):
-            return self[args]
-        def __missing__(self, key):
-            ret = self[key] = self.f(*key)
-            return ret
-    return memodict(f)
+from memoisation import memoise
 
 # Set of singletons
 I = set()
-#D = set()
+
+# Sample
 D = list()
+
 # Summary:
 C = set()
 
 # Set of all attributes:
 A = 'abcdef'
-print 'Attributes: ', A
+print 'Attributes (%d): %s' % (len(A), A)
 
 def create_patterns(attributes, patterns, size):       
     """Creates all patterns of a given size and add them to patterns"""
@@ -44,43 +35,44 @@ create_patterns(A, patterns, 3)
 #Add all singletons to sumary:
 create_patterns(A, C, 1)
 
-
-print 'legth of A:', len(A)
+# Generate random sample D
 while len(D) != 300:
     _sample = sample(A, randint(1, len(A)))
     D.append(''.join(sorted(_sample)))
-print 'D: ', D
+print 'D (%d): %s' % (len(D), D)
 
 for index, x in enumerate(D):
     D[index] = to_binary(x)
 
+# Initialise sample space
 T  = 2 ** len(A)
-# Dict for values u_x
+
+# Dict for values U_x
 U = {} 
-u0 = 0
+u0 = 2 ** -len(A)
 
 def contains(a, b):
     """ True if a contains b """
     return a & b == b
 
-def model(t, _C, _u0, _U):
+def model(t, C, u0, U):
     # t = abc
     res = 1.0
-    for x in _C:
+    for x in C:
         # x = ab
         if contains(t, x):
-            res = res * _U[x]
-    return _u0 * res
+            res = res * U[x]
+    return u0 * res
 
-def query(x, _C, _u0, _U):
-    # x = ab
+def query(x, C, u0, U):
     p = 0.0
     for t in range(T):
         if contains(t, x):
-            # t = abc
-            p += model(t, _C, _u0, _U)
+            p += model(t, C, u0, U)
     return p
 
+# Memoise: The function will cache previous results with the argument
+# this assumes that D does not change between cached results.
 @memoise
 def fr(x):
     p = 0.0
@@ -91,40 +83,31 @@ def fr(x):
     assert p <= 1.0
     return p
 
-def printU(U):
-    for k in U:
-        print to_chars(k) + ':' + str(U[k])
-
-def iterative_scaling(_C):
-    _U = {}
-    _u0 = 2 ** -len(A)
-    biggest_diff = -1
-    for c in _C:
-        _U[c] = 1.0
+def iterative_scaling(C):
+    U = {}
+    u0 = 2 ** -len(A)
+    biggestdiff = -1
+    for c in C:
+        U[c] = 1.0
     converge_iterations = 0
     iterations = 0
     while iterations < 20: #((biggest_diff > 0.00000001 or biggest_diff == -1) and iterations < 100): # converge
         converge_iterations += 1
         biggest_diff = -1
         iterations += 1
-        for x in _C:
+        for x in C:
 
-            p = query(x, _C, _u0, _U)
-            _U[x] = _U[x] * (fr(x) / p) * ((1 - p) / (1 - fr(x)))
-            _u0 = _u0 * (1 - fr(x)) / (1 -  p)
+            p = query(x, C, u0, U)
+            U[x] = U[x] * (fr(x) / p) * ((1 - p) / (1 - fr(x)))
+            u0 = u0 * (1 - fr(x)) / (1 -  p)
 
-            diff = abs(fr(x) - query(x, _C, _u0, _U))
+            diff = abs(fr(x) - query(x, C, u0, U))
             if diff > biggest_diff:
                 biggest_diff = diff
                 # print 'biggest_diff:%f fx:%f p:%f' % (biggest_diff, fr(x), p)
 
     # print 'Converge iterations:%d biggest_diff:%f ' % (converge_iterations, biggest_diff)
-    return _u0, _U
-
-def run():
-    """ Query all x in C subject to U """
-    for x in C:
-        print to_chars(x) + ':', query(x, u0, U)
+    return u0, U
 
 def find_best_itemset():
     """Returns a pattern that potentially will be included in the summary."""
@@ -135,15 +118,36 @@ def MTV():
     global C
     global u0
     global U
+
+    # Compute our initial, current best, model
     u0, U = iterative_scaling(C)
+
+    # This is the current best score
+    cur_score = s(C, u0, U)
+
+    # Brute force all patterns
     while len(patterns) > 0: 
-        X = find_best_itemset() 
+
+        # Possible best itemset to include in the summary
+        X = find_best_itemset()
+
+        # Candidate summary 
         _C = C.union([X])
+        
+        # Candidate model
         _u0, _U = iterative_scaling(_C) 
-        if s(_C, _u0, _U) < s(C, u0, U):
-            C = copy.deepcopy(_C)
+        
+        # Candidate score
+        temp_score = s(_C, _u0, _U)
+
+        # If the score has improved, we use the 
+        # candidate summary as our
+        # current best summary
+        if  temp_score < cur_score:
+            cur_score = temp_score
+            C = _C
             u0 = _u0
-            U = copy.deepcopy(_U)
+            U = _U
 
 def s(C, u0, U):
     return -1 * (len(D) * (log(u0) +  sum([fr(x) * log(U[x]) for x in C]))) + 0.5 * len(C) * log(len(D))
@@ -155,22 +159,21 @@ print 'Final summary: '
 for x in C:
     print to_chars(x)
 
-# print 'frequency of %s: %f ' % (to_chars(ab), fr(ab))
-# iterative_scaling()
 for c in C:
     print 'query %s with fr %f query %f uX: %f' % (to_chars(c), fr(c), query(c, C, u0, U), U[c])
 print 'u0: ', u0
-# print 'query not in set %s with fr %f query %f ' % (to_chars(_def), fr(_def), query(_def))
-# print 'C', C
-# print 'u0', u0
 
-def is_in_sumamry(y, _C):
+def is_in_sumamry(y, C):
     for x in C:
         if y == x:
             return True
     return False
 
 def query_unknowns(amount):
+    """Attempts to create an amount of itemsets not in the summary, and
+        print their frequency and estimated frequency
+        amount: Amount of itemsets not in C to attempt to find
+    """
     unknowns = 0
     for t in range(T):
         y = randint(0, T)
@@ -183,22 +186,17 @@ def query_unknowns(amount):
 query_unknowns(10)
 
 def total_probability():
+    """ Assert and print the total probability of the model """
     total_prob = 0.0
     for t in range(T):
         p = model(t, C, u0, U)
-        # print 't: %s prob: %f' % (to_chars(t), p)
-        # if p == u0:
-        #     print 'prob of %s equals u0' % to_chars(t)
         total_prob += p
+    assert abs(total_prob - 1.0) < 0.001
     print 'total prob: ', total_prob
 total_probability()
-# cdf = to_binary('c')
-# print 'prop for no seen %s:%f' % (to_chars(cdf), query(cdf))
-# for xi in X:
-#     print '%s contains ab %d' % (to_chars(xi), contains(xi, ab))
 
 # def running_example():
-#     # Summary from running example
+#     # Summary from running example from Mampey et. al
 #     abc  = to_binary('abc') # 00000111
 #     cd   = to_binary('cd')  # 00001100
 #     _def = to_binary('def') # 00111000
