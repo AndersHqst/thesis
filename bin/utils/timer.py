9243433315a328ffaaab3a849264813b63ba8afc
AndersHqst <anders.host.kjaergaard@gmail.com>
2015-01-22 16:26:15 +0100

    Cleanup and better folder structure. Added Faust result parser that returns a list of FaustResult objects

---
+++ bin/utils/timer.py
@@ -0,0 +1,21 @@
+from time import time
+timings = {}
+starts = {}
+
+def timer_start(flag):
+    global start
+    global starts
+
+    starts[flag] = time()
+
+def timer_stop(flag):
+    global timings
+    global starts
+
+    if not flag in timings:
+        timings[flag] = 0
+    timings[flag] = timings[flag] + (time() - starts[flag])
+
+def timer_print_timings():
+    for flag in timings:
+        print '[TIMER] %s: %f' % (flag, timings[flag])
