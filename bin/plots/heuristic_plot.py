e4b44ca48016ce4accefac0839c75688048083ca
AndersHqst <anders.host.kjaergaard@gmail.com>
2015-01-23 12:45:52 +0100

    Added ignore for full data set file hmp.lq.phylotype.filter.tab, add this locally. We should only store subsamples in git

---
+++ bin/plots/heuristic_plot.py
@@ -0,0 +1,14 @@
+
+"""
+    Used in ipython
+    ./ipython qtconsole --pylab=inline
+"""
+# h_50 = [h(0.5, (i+10)/100.) for i in range(80)]
+# ylabel('h(0.5,y)')
+# xlabel('y')
+# plot([(i+10)/100. for i in range(80)], h_50)
+# savefig('h_function.png')
+
+
+
+
